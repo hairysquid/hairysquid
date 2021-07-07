d1c6a35f556324d493777671ad86281fe21e8d1d
Ændrew Rininsland <aendrew@aendrew.com>
2021-07-07 15:28:17 +0100

    Fixing dep

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
 import "../styles/globals.css";
-import { StaticKitProvider } from "@statickit/react";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <StaticKitProvider site="de24cb33b4b7">
-      <Component {...pageProps} />
-    </StaticKitProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
