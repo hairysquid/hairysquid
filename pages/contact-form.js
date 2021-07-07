19a65b466f4fe1c709bbdcd6ccdfae90b9800fcc
aendrew <aendrew@aendrew.com>
2021-07-07 01:43:15 +0100

    Fixing form

--- pages/contact-form.js
+++ pages/contact-form.js
@@ -1,23 +1,9 @@
 import styles from "../styles/contact-form.module.css";
 import React, { useState, useEffect, useRef } from "react";
-import { useForm, ValidationError } from "@statickit/react";
-import { ReCaptcha, loadReCaptcha } from "react-recaptcha-v3";
+import { useForm, ValidationError } from "@formspree/react";
 
 export default function ContactForm() {
-  const [state, handleSubmit] = useForm("contactForm");
-  const [captchaSuccess, setCaptchaSuccess] = useState(false);
-  const [recaptchaReady, setRecaptchaReady] = useState(false);
-  const reCaptchaRef = useRef(null);
-
-  useEffect(() => {
-    try {
-      loadReCaptcha(process.env.NEXT_PUBLIC_WEBSITE_KEY, () =>
-        setRecaptchaReady(true)
-      );
-    } catch (e) {
-      console.error(e);
-    }
-  }, []);
+  const [state, handleSubmit] = useForm("xqkwlonj");
 
   if (state.succeeded) {
     return <p>Thanks for messaging us, we will get back to you shortly</p>;
@@ -53,14 +39,6 @@ export default function ContactForm() {
             errors={state.errors}
           />
         </div>
-        <ReCaptcha
-          ref={reCaptchaRef}
-          sitekey={process.env.NEXT_PUBLIC_WEBSITE_KEY}
-          action="sendEmail"
-          verifyCallback={() => {
-            setCaptchaSuccess(true);
-          }}
-        />
         <div className={styles.label}>
           {captchaSuccess && (
             <button
